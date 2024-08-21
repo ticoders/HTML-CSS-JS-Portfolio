860517f72f4b08f81d06294811e3182063e4830d
tidiong <62657965+tidiong@users.noreply.github.com>
2024-08-21 01:40:30 -0500

    Updated projects and fixed some bugs in design.

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-
-function toggleMenu(){
-
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open"); 
-    icon.classList.toggle("open"); 
 
+    if (menu && icon) {
+        menu.classList.toggle("open"); 
+        icon.classList.toggle("open"); 
+    }
 }
+
+// Add event listener to handle clicks
+document.querySelector(".hamburger-icon").addEventListener("click", toggleMenu);
+
 
 
